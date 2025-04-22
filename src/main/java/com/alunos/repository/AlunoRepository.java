a23a8454b29bcba2d2320601cc2607c17120bc59
Vitu Felipe <victor_felipe28@hotmail.com>
2025-04-22 14:52:24 -0300

    configs front

--- src/main/java/com/alunos/repository/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alunos.repository;
-
-import com.alunos.model.Aluno;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-// aqui é pra fornecer os metodos prontos de save find all
-public interface AlunoRepository extends MongoRepository<Aluno, String> {
-}
