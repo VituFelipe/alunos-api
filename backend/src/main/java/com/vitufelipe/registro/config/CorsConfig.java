8c4176e0c71f933102a30e66a09cac1bb313073a
Vitu Felipe <victor_felipe28@hotmail.com>
2025-04-22 15:45:21 -0300

    cors config

--- backend/src/main/java/com/vitufelipe/registro/config/CorsConfig.java
+++ backend/src/main/java/com/vitufelipe/registro/config/CorsConfig.java
@@ -2,9 +2,10 @@ package com.vitufelipe.registro.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
